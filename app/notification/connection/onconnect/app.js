d0db917210d47538e0ed69514ba53caf05073c71
Alberto Meneghini <menalb84@gmail.com>
2024-07-08 19:41:11 +0200

    fixed notification

--- app/notification/connection/onconnect/app.js
+++ app/notification/connection/onconnect/app.js
@@ -1,22 +1,25 @@
-const AWS = require('aws-sdk');
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
 
-const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
+const client = new DynamoDBClient({ region: process.env.AWS_REGION });
+// const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
+const ddb = DynamoDBDocumentClient.from(client);
 
 exports.handler = async event => {
     let userId = "";
     if (event.requestContext.authorizer) {
         userId = event.requestContext.authorizer.userId;
     }
-    const putParams = {
+    const putParams = new PutCommand({
         TableName: process.env.TABLE_NAME,
         Item: {
             connectionId: event.requestContext.connectionId,
             userId: userId
         }
-    };
+    });
 
     try {
-        await ddb.put(putParams).promise();
+        await ddb.send(putParams);
     } catch (err) {
         return { statusCode: 500, body: 'Failed to connect: ' + JSON.stringify(err) };
     }
